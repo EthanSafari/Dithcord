468c7808b62270a4b2477c10cdc8beea7d6f891d
Alex <acastro166@gmail.com>
2023-01-14 13:02:39 -0800

    completed server-seeder (REVIEW)

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -5,27 +5,29 @@ def seed_servers():
     
     server1 = Server(
         name = 'Mike Tyson Mysteries',
-        server_image = '/static/images/dithcord_server_image.png',
+        server_image = '/static/images/server_images/dithcord_server_image.png',
         owner_id = 1,
     )
     
     server2 = Server(
         name = 'Boxing',
-        server_image = '',
+        server_image = '/static/images/server_images/tysonBoxing_server_image.png',
         owner_id = 2,
     )
     
     server3 = Server(
         name = 'Tyson\'s Tigers',
-        server_image = '',
+        server_image = '/static/images/server_images/tysonTiger_server_image.png',
         owner_id = 3,
     )
     
     server4 = Server(
         name = 'Face Tattoos',
-        server_image = '',
+        server_image = '/static/images/server_images/tysonTattoo_server_image.jpg',
         owner_id = 4,
     )
     
 
+def undo_server():
+    db.session.execute("DELETE FROM servers")
     
