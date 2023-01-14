4f8fa6aeab118bb954ef72eaf0f6fc35c56b7b64
Ethan <eman5797@gmail.com>
2023-01-14 16:28:53 -0500

    db init successful

--- app/models/server.py
+++ app/models/server.py
@@ -3,8 +3,8 @@ from .db import db
 
 server_users = db.Table(
     'server_users',
-    db.Column('server_id', db.Integer, db.ForeignKey('server.id'), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    db.Column('server_id', db.Integer, db.ForeignKey('servers.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
 )
 
 class Server(db.Model):
