ba672b3b88d90b32ed6e6f3ee6796f53494feca7
Ethan <eman5797@gmail.com>
2023-01-14 16:04:28 -0500

    added seeding commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .channels import seed_channels, undo_channels
+from .messages import seed_message_images, seed_messages, undo_message_images, undo_messages
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,17 +14,26 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_message_images()
+        undo_messages()
+        undo_channels()
         undo_users()
     seed_users()
     # Add other seed functions here
+    seed_channels()
+    seed_messages()
+    seed_message_images()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
     # Add other undo functions here
+    undo_message_images()
+    undo_messages()
+    undo_channels()
+    undo_users()
