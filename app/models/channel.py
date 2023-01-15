79144ccf6dc658e059b878b84a2bd00d68a16f26
Ethan <eman5797@gmail.com>
2023-01-15 18:29:24 -0500

    altered models for production

--- app/models/channel.py
+++ app/models/channel.py
@@ -1,11 +1,14 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Channel(db.Model):
     __tablename__ = 'channels'
+
+    if environment == "production": __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     private = db.Column(db.Boolean, default=False, nullable=False)
     name = db.Column(db.String(255), nullable=False)
-    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'), nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
 
     messages = db.relationship('Message', back_populates='channel', cascade="all, delete")
     server = db.relationship('Server', back_populates='channels')
--- app/models/message.py
+++ app/models/message.py
@@ -1,12 +1,15 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 import datetime
 
 class Message(db.Model):
     __tablename__ = 'messages'
+
+    if environment == "production": __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(750))
-    channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    channel_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('channels.id')), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
