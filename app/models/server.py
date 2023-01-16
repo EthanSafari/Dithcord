807b56e2909e9be3629f5a28727d9390f6618a54
Ethan <eman5797@gmail.com>
2023-01-15 22:10:53 -0500

    please work this time... you worked locally :,)

--- app/models/server.py
+++ app/models/server.py
@@ -4,8 +4,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 server_users = db.Table(
     'server_users',
     db.Model.metadata,
-    db.Column('server_id', db.Integer, db.ForeignKey('servers.id'), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True))
+    db.Column('server_id', db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id)'), primary_key=True))
 
 if environment == 'production':
     server_users.schema = SCHEMA
