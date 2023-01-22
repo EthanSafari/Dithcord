38098487bcc5c336c604e2dddf7753eef36db9e1
Ethan <eman5797@gmail.com>
2023-01-21 21:44:41 -0500

    added logout

--- react-app/src/components/dithcord/Channels/Channels.js
+++ react-app/src/components/dithcord/Channels/Channels.js
@@ -6,39 +6,54 @@ import AddChannelFormModal from '../Forms/ChannelForm/Add/AddChannelFormModal';
 import EditChannelFormModal from '../Forms/ChannelForm/Edit/EditChannelFormModal';
 import ServerDropDownMenu from '../Servers/ServerDropDownMenu';
 import DeleteChannelButton from './DeleteChannelButton';
+import LogoutButton from '../../auth/LogoutButton';
 
 function Channels({ channels }) {
     const dispatch = useDispatch();
     const currentChannels = Object.values(channels)
-    // const currentServerObj = useSelector(state => state.servers.oneServer)
-    // const currentServer = Object.values(currentServerObj)
+    const currentServerObj = useSelector(state => state.servers.oneServer)
+    const currentServer = Object.values(currentServerObj)
+    const sessionUser = useSelector(state => state.session.user)
 
+    console.log(currentServerObj)
     // console.log('INSIDE OF CHANNELS COMPONENT', currentServer[0]?.id)
 
     const getOneChannel = (channelId) => {
-        if(channelId) {
+        if (channelId) {
             dispatch(getChannel(channelId))
             dispatch(getChannelMessages(channelId))
         }
     }
 
     return (
-        <div>
-            <ServerDropDownMenu />
-                <AddChannelFormModal />
-            <div className='channels-container'>
-            {currentChannels && currentChannels.map((channel) => (
-                <div key={channel.id} className='channel-and-channel-options'>
-                    <div onClick={() => getOneChannel(channel.id)} key={channel.id}>
-                        {/* {console.log('', '\n', '--------------CHANNELS COMPONENT DATA--------------', '\n', channel, '\n', '')} */}
-                        <h2 key={channel.id}>{channel.name}</h2>
-                    </div>
-                    <div>
-                        <EditChannelFormModal />
-                        <DeleteChannelButton key={channel.id} channelId={channel.id}/>
-                    </div>
+        <div className='channel-box'>
+            <div>
+                <div className='server-name'>
+                    {currentServer[0]?.name}
                 </div>
-            ))}
+                <ServerDropDownMenu />
+                <AddChannelFormModal />
+            </div>
+            <div className='channels-container'>
+                {currentChannels && currentChannels.map((channel) => (
+                    <div key={channel.id} className='channel-and-channel-options'>
+                        <div onClick={() => getOneChannel(channel.id)} key={channel.id}>
+                            {/* {console.log('', '\n', '--------------CHANNELS COMPONENT DATA--------------', '\n', channel, '\n', '')} */}
+                            <h2 key={channel.id}>{channel.name}</h2>
+                        </div>
+                        <div>
+                            <EditChannelFormModal />
+                            <DeleteChannelButton key={channel.id} channelId={channel.id} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='user-info'>
+                <div>
+                    <img src={sessionUser.profile_img} alt={sessionUser.username} />
+                    {sessionUser.username}
+                </div>
+                    <LogoutButton />
             </div>
         </div>
     )
